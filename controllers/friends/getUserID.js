05872369592f67f8b0b69431021a622193108190
jihyun <chiiclaro@gmail.com>
2024-05-31 21:20:26 +0900

    Feat: user 서비스와의 통신

---
+++ controllers/friends/getUserID.js
@@ -0,0 +1,18 @@
+//userid를 통신을 통해 가져올 수 있게. 
+const http = require("http");
+const { friendsDB } = require("../../models");
+
+module.exports = async(req, res) => {
+    
+    try{
+        
+
+
+
+    }
+
+    catch(err){
+        console.log(err);
+    }
+
+}
